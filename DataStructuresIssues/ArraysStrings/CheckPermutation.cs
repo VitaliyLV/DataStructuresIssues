4dd2e459b4083a40a7d810a13af583cd4757d298
VitaliyLV <vitaliy.zubenyuk@gmail.com>
2023-12-30 20:12:03 +0200

    add permutations

---
+++ DataStructuresIssues/ArraysStrings/CheckPermutation.cs
@@ -0,0 +1,63 @@
+﻿namespace DataStructuresIssues.ArraysStrings
+{
+    internal class CheckPermutation : ISolveIssue
+    {
+        const string trueResponse = "TRUE. The string are permutations";
+        const string falseResponse = "FALSE. The strings are not permutations";
+
+        //Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+        public void Solve()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter 2 strings to check if one is permutation on the other:");
+                var str1 = Console.ReadLine();
+                if (str1 == Helper.QuitStr)
+                    return;
+
+                var str2 = Console.ReadLine();
+
+                Console.WriteLine("Method1: " + (IsPermutation1(str1, str2) ? trueResponse : falseResponse));
+                Console.WriteLine("Method2: " + (IsPermutation2(str1, str2) ? trueResponse : falseResponse));
+            }
+        }
+        //Compare number of each chars
+        //O(n)
+        private bool IsPermutation1(string? str1, string? str2)
+        {
+            if (str1 == null || str2 == null || str1.Length != str2.Length)
+                return false;
+            int[] chars1 = new int[128];
+            int[] chars2 = new int[128];
+            for (int i = 0; i < str1.Length; i++)
+            {
+                chars1[str1[i]]++;
+                chars2[str2[i]]++;
+            }
+            for (int i = 0; i < 128; i++)
+            {
+                if (chars1[i] != chars2[i])
+                    return false;
+            }
+
+            return true;
+        }
+        //sort and compare
+        private bool IsPermutation2(string? str1, string? str2)
+        {
+            if (str1 == null || str2 == null || str1.Length != str2.Length)
+                return false;
+            var sortS1 = Sort(str1);
+            var sortS2 = Sort(str2);
+            if (sortS1! != null && sortS2 != null)
+                return sortS1.Equals(sortS2);
+            return false;
+        }
+        private string? Sort(string str)
+        {
+            char[] cArray = str.ToCharArray();
+            Array.Sort(cArray);
+            return new string(cArray);
+        }
+    }
+}
---
+++ DataStructuresIssues/ArraysStrings/ContainsPermutation.cs
@@ -0,0 +1,54 @@
+﻿namespace DataStructuresIssues.ArraysStrings
+{
+    internal class ContainsPermutation : ISolveIssue
+    {
+        const string trueResponse = "TRUE. The string are permutations";
+        const string falseResponse = "FALSE. The strings are not permutations";
+
+        //LeetCode 567. Permutation in String
+        //Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+        //In other words, return true if one of s1's permutations is the substring of s2.
+        public void Solve()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter 2 strings to check if one is permutation on the other:");
+                var str1 = Console.ReadLine();
+                if (str1 == Helper.QuitStr)
+                    return;
+
+                var str2 = Console.ReadLine();
+                if (str1 == null || str2 == null)
+                {
+                    Console.WriteLine(falseResponse);
+                    return;
+                }
+                Console.WriteLine("Method1: " + (CheckInclusion(str1, str2) ? trueResponse : falseResponse));
+            }
+        }
+        //Resolve by sorting
+        public bool CheckInclusion(string s1, string s2)
+        {
+            if (s2.Length < s1.Length)
+                return false;
+            if (s2.Contains(s1))
+                return true;
+            string sortedS1 = Sort(s1);
+            for (int i = 0; i <= s2.Length - s1.Length; i++)
+            {
+                var sortedS2 = Sort(s2.Substring(i, s1.Length));
+                if (sortedS1.Equals(sortedS2))
+                    return true;
+            }
+
+            return false;
+        }
+        private string Sort(string str)
+        {
+            char[] cArray = str.ToCharArray();
+            Array.Sort(cArray);
+            return new string(cArray);
+        }
+
+    }
+}
