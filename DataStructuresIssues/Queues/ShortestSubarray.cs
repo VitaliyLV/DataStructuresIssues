518d0d9553b3ccd1ac6d2adf3007ae971d5b9923
VitaliyLV <vitaliy.zubenyuk@gmail.com>
2024-01-06 15:14:08 +0200

    add new solution to shortest subarray

--- DataStructuresIssues/Queues/ShortestSubarray.cs
+++ DataStructuresIssues/Queues/ShortestSubarray.cs
@@ -9,18 +9,21 @@
         /// </summary>
         public void Solve()
         {
-            //int[] nums = new int[] { 1,2,5,4,-2,1,-4 };
+            //int[] nums = new int[] { -2, 1,2,5,4,-2,1,-4 };
             var nums = Helper.ReadNumbers();
             if(nums == null) 
                 return;
             Console.WriteLine("Enter minimal searched sum:");
             var numStr = Console.ReadLine();
+            var numsArray = nums.ToArray();
             if (int.TryParse(numStr, out var num))
             {
-                Console.WriteLine("Slower method: " + ShortestSubSlower(nums.ToArray(), num));
-                Console.WriteLine("Faster mathod: " + ShortestSubFaster(nums.ToArray(), num));
+                Console.WriteLine("Slower method: " + ShortestSubSlower(numsArray, num));
+                Console.WriteLine("Faster mathod: " + ShortestSubFaster(nums, num));
+                Console.WriteLine("Faster mathod 2: " + ShortestFromLeet(numsArray, num));
             }
         }
+       
         //resolve by adding numbers in 3 loops
         public int ShortestSubSlower(int[] nums, int k)
         {
@@ -45,15 +48,14 @@
             return -1;
         }
         //resolve by saving added values on each step and removing negative numbers from both ends
-        public int ShortestSubFaster(int[] nums, int k)
+        public int ShortestSubFaster(List<int> numbers, int k)
         {
-            if (nums.Length == 0)
+            if (numbers.Count == 0)
                 return -1;
-            if (nums.Any(x => x >= k))
+            if (numbers.Any(x => x >= k))
                 return 1;
 
-            List<int> numbers = new List<int>(nums);
-            List<int> sumArray = new List<int>(nums);
+            List<int> sumArray = new List<int>(numbers);
             
             RemoveNegatives(ref sumArray, ref numbers);
             int minNum = 2;
@@ -86,7 +88,28 @@
                 numbers.RemoveAt(0);
             }
         }
+        //resolve by adding all numbers and popping indexes of smaller sums
+        public int ShortestFromLeet(int[] A, int K)
+        {
+            int N = A.Length, res = N + 1;
+            long[] B = new long[N + 1];
+            for (int i = 0; i < N; i++) B[i + 1] = B[i] + A[i];
+            LinkedList<int> d = new LinkedList<int>();
+            for (int i = 0; i < N + 1; i++)
+            {
+                while (d.Count > 0 && B[i] - B[d.First()] >= K)
+                {
+                    res = Math.Min(res, i - d.First());
+                    d.RemoveFirst();
+                }
+                while (d.Count > 0 && B[i] <= B[d.Last()])
+                {
+                    d.RemoveLast();
+                }
+                d.AddLast(i);
+            }
+            return res <= N ? res : -1;
+        }
 
-        
     }
 }
